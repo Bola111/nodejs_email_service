eb876863ed5875eea1b4a529b6901ca9dea6f7ee
bolathedev <godswillchibuzororie@gmail.com>
2021-02-23 12:12:24 +0100

    latest changes

--- server/api/User/routes.js
+++ server/api/User/routes.js
@@ -2,6 +2,5 @@ const router = require("express").Router();
 const userController = require("./controller");
 
 router.post("/", userController.registerNewUser);
-router.get("/", userController.getAllUsers);
 
 module.exports = router;
