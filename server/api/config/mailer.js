90b61d22423db1573a0ad865c3689454f05c0a8c
bolathedev <godswillchibuzororie@gmail.com>
2021-02-23 11:16:13 +0100

    latest changes

--- server/api/config/mailer.js
+++ server/api/config/mailer.js
@@ -3,9 +3,12 @@ const hbs = require("nodemailer-express-handlebars")
 
 let transporter = nodemailer.createTransport({
     service: 'Mailgun',
+    port: process.env.MAILGUN_SMTP_PORT,
     auth: {
-      user: process.env.MAILGUN_SMTP_LOGIN,
-      pass: process.env.MAILGUN_SMTP_PASSWORD
+        user: process.env.MAILGUN_SMTP_LOGIN,
+        pass: process.env.MAILGUN_SMTP_PASSWORD,
+        api_key: process.env.MAILGUN_API_KEY,
+        domain: process.env.MAILGUN_DOMAIN
     }
 });
 const handlebarOptions = {
