196ddfabcb05ecca9572d65163c74feedc96abec
bolathedev <godswillchibuzororie@gmail.com>
2021-02-28 16:24:18 +0100

    latest-changes

--- server/api/User/controller.js
+++ server/api/User/controller.js
@@ -93,12 +93,19 @@ exports.createOtp = async (req, res) => {
         const OTP = Math.floor(100000 + Math.random() * 900000)
         const d = new Date();
         const v = new Date();
-        firebase.database.collection('otp').add({
-            otp: OTP,
-            email: req.body.email,
-            date: Date.now(),
-            expiry: v.setMinutes(d.getMinutes() + 10)
+        firebase.database.collection('otp').where('email', '==', req.body.email).get().then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                doc.ref.delete()
+            })
         })
+        setTimeout(() => {
+            firebase.database.collection('otp').add({
+                otp: OTP,
+                email: req.body.email,
+                date: Date.now(),
+                expiry: v.setMinutes(d.getMinutes() + 10)
+            })
+        }, 2000)
         mailer.sendOTP(req.body.name, req.body.email, OTP)
         res.status(200).json({
             message: 'One Time Password Has Been Sent',
