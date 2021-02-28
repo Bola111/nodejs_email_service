b7dd44d48904ad4b5b869f87b5ab9ecf0faf33c1
bolathedev <godswillchibuzororie@gmail.com>
2021-02-28 10:51:42 +0100

    latest-changes

--- server/api/routeHandler.js
+++ server/api/routeHandler.js
@@ -2,7 +2,7 @@ const userRoute = require("./User/routes");
 const depositRoute = require("./User/deposits.js")
 const approvedRoute = require("./User/approved.js")
 const adminnotifyRoute = require("./User/depositnotify.js")
-const declinedRoute = require("./User/declinedDeposit.js")
+const declinedRoute = require("./User/declined.js")
 module.exports = (app) => {
     app.use("/user", userRoute);
     app.use("/newdeposit", depositRoute);
